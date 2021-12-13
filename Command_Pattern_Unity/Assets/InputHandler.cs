d705126eb671c63cd4c81fe027ba2c75f699c2af
Celil Eren Kalkan <60541083+CelilErenKalkan@users.noreply.github.com>
2021-12-13 17:09:49 +0300

    Practice
    My practice with Command Pattern in Unity, working on movement commands.

---
+++ Command_Pattern_Unity/Assets/InputHandler.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator anim;
+    private Command keyQ, keyW, keyE, keyP, keyK, upArrow;
+    private List<Command> oldCommands = new List<Command>();
+
+    Coroutine replayCoroutine;
+    bool shouldStartReplay;
+    bool isReplaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyQ = new Performjump();
+        keyW = new DoNothing();
+        keyE = new DoNothing();
+        keyP = new PerformPunch();
+        keyK = new PerformKick();
+        upArrow = new MoveForward();
+        anim = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isReplaying)
+            HandleInput();
+        StartReplay();
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            keyQ.Execute(anim, true);
+            oldCommands.Add(keyQ);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            keyW.Execute(anim, true);
+            oldCommands.Add(keyW);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            keyE.Execute(anim, true);
+            oldCommands.Add(keyE);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            keyP.Execute(anim, true);
+            oldCommands.Add(keyP);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            keyK.Execute(anim, true);
+            oldCommands.Add(keyK);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            upArrow.Execute(anim, true);
+            oldCommands.Add(upArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            shouldStartReplay = true;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            UndoLastCommand();
+    }
+
+    void UndoLastCommand()
+    {
+        if (oldCommands.Count > 0)
+        {
+            Command c = oldCommands[oldCommands.Count - 1];
+            c.Execute(anim, false);
+            oldCommands.RemoveAt(oldCommands.Count - 1);
+        }
+    }
+
+    void StartReplay()
+    {
+        if (shouldStartReplay && oldCommands.Count > 0)
+        {
+            shouldStartReplay = false;
+            if(replayCoroutine != null)
+                StopCoroutine(replayCoroutine);
+            replayCoroutine = StartCoroutine(ReplayCommands());
+        }
+    }
+
+    IEnumerator ReplayCommands()
+    {
+        isReplaying = true;
+
+        for (int i = 0; i > oldCommands.Count; i++)
+        {
+            oldCommands[i].Execute(anim, true);
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        isReplaying = false;
+    }
+}
